3f4d1a96a4f652bfa4fd2b23b6731570c21ec909
GaboAfk <gabrielmorffe95@gmail.com>
2024-03-19 17:45:31 -0300

    INT_MIN case

--- print_numbers.c
+++ print_numbers.c
@@ -1,12 +1,13 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * print_decR - auxiliar function that prints a decimal number recursivemt.
  * @var: list of arguments.
- * @count: numbers printed.
+ * @count: pointer to numbers count.
  * Return: 0 Success. 5 Error.
  */
-void print_decR(int var, int count)
+void print_decR(int var, int *count)
 {
 	char aux;
 
@@ -14,8 +15,8 @@ void print_decR(int var, int count)
 		print_decR(var / 10, count);
 
 	aux = (var % 10) + '0';
-	write(1, &aux, sizeof(char));
-	count++;
+	write(STDOUT_FILENO, &aux, sizeof(char));
+	*count += 1;
 }
 
 /**
@@ -27,16 +28,20 @@ int print_dec(va_list var)
 {
 	int aux = va_arg(var, int);
 	int count = 0;
+	char *intmin = "-2147483648";
 
-/*	if (!aux)*/
-/*		return (4);*/
+	if (aux == INT_MIN)
+	{
+		return (write(1, intmin, 11));
+	}
 
 	if (aux < 0)
 	{
 		write(1, "-", sizeof(char));
+		count++;
 		aux = -aux;
 	}
 
-	print_decR(aux, count);
+	print_decR(aux, &count);
 	return (count);
 }
